c1c783f7621c59490dedbed93d78c8138906c99a
dnandha <daljeet@gmx.net>
2022-06-23 23:54:25 +0200

    fix license

--- lib/java/nbauth/Crypto.java
+++ lib/java/nbauth/Crypto.java
@@ -1,3 +1,25 @@
+//
+//  MiAuth - Authenticate and interact with Xiaomi devices over BLE
+//  Copyright (C) 2022  Daljeet Nandha
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+//
+//  This class is my Java port of https://github.com/scooterhacking/NinebotCrypto
+//  Huge thanks to the original authors for sharing their work!
+//
+
 package de.nandtek.nbauth;
 
 import java.security.InvalidKeyException;
